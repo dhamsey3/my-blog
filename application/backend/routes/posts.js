e1b2a60df0f624202f341a1b28448371a3b37d90
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-06-18 15:39:07 +0200

    Update posts.js

--- application/backend/routes/posts.js
+++ application/backend/routes/posts.js
@@ -1,1 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+// Retrieve all posts
+router.get("/", (req, res) => {
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.error("Error fetching posts:", err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.json(posts);
+    }
+  });
+});
+
+// Create a new post
+router.post("/", (req, res) => {
+  const { title, content } = req.body;
+
+  const newPost = new Post({
+    title,
+    content
+  });
+
+  newPost.save()
+    .then(post => res.json(post))
+    .catch(err => {
+      console.error("Error saving post:", err);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+module.exports = router;
 
